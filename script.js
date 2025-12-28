6dba7a8b9d4025d1dc15be6822f2b4a8b88a6e4f
aayushapaudel18-sudo <aayushapaudel18@gmail.com>
2025-12-28 14:09:16 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+ // menu or hamburger toggle
+const menu = document.getElementById("menu");
+const navbar = document.getElementById("navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+
+// form validation
+function validate() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if (!name || name.length < 8) {
+    alert("Your Name must be at least 8 characters.");
+    return false;
+  }
+
+  if (!email.includes("@")) {
+    alert("Invalid Email");
+    return false;
+  }
+
+  if (!password || password.length < 6) {
+    alert("Invalid password");
+    return false;
+  }
+
+  alert("Form Submitted Successfully.");
+  return true;
+}
